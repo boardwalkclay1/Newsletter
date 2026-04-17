fd87a5792a77e9b708deb3f40fcedb5898be4eff
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-17 19:02:53 -0400

    Create mainNesletterData.js

---
+++ src/mainNesletterData.js
@@ -0,0 +1,52 @@
+import {
+  getLocalRinkEvents,
+  getNationalRinkEvents,
+  getGlobalRinkEvents,
+  splitLocalByType
+} from './rinkEvents.js';
+import { getRandomSegment } from './beltlineMap.js';
+import { getRandomRestaurants } from './beltlineRestaurants.js';
+
+export async function buildNewsletterData(options = {}) {
+  const {
+    segmentId = null,
+    restaurantCount = 3,
+    manualContent = {}
+  } = options;
+
+  const beltlineMap = getRandomSegment();
+  const beltlineRestaurants = getRandomRestaurants(restaurantCount, segmentId || beltlineMap?.id);
+
+  const localEventsRaw = getLocalRinkEvents();
+  const { adultNights, familyKids, major } = splitLocalByType(localEventsRaw);
+
+  const rinkEventsNational = getNationalRinkEvents();
+  const rinkEventsGlobal = getGlobalRinkEvents();
+
+  return {
+    title: manualContent.title || 'Boardwalk Newsletter',
+    dateTime: manualContent.dateTime || new Date().toLocaleString(),
+    paragraphs: manualContent.paragraphs || [],
+    sections: manualContent.sections || [],
+    ending: manualContent.ending || '',
+    links: manualContent.links || [],
+    qrCodes: manualContent.qrCodes || [],
+    video: manualContent.video || { type: 'none' },
+
+    beltlineMap,
+    beltlineRestaurants,
+
+    rinkEventsLocalAdult: adultNights,
+    rinkEventsLocalFamily: familyKids,
+    rinkEventsLocalMajor: major,
+    rinkEventsNational,
+    rinkEventsGlobal,
+
+    theme: manualContent.theme || 'old-scroll',
+    fontStyle: manualContent.fontStyle || 'old-english',
+    textColor: manualContent.textColor || '#2b1b0f',
+    textBgColor: manualContent.textBgColor || '#fdf5e6',
+    showLogo: manualContent.showLogo ?? true,
+    showSignature: manualContent.showSignature ?? true
+  };
+}
