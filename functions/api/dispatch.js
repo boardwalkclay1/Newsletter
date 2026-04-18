67bf39e48b9e7e01a7fc24b97825b9836227922c
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-18 18:46:50 -0400

    Create dispatch.js

---
+++ functions/api/dispatch.js
@@ -0,0 +1,21 @@
+export async function onRequestPost(context) {
+  const { action, payload } = await context.request.json();
+
+  const token = context.env.GITHUB_PAT;
+  const owner = context.env.GITHUB_OWNER;
+  const repo = context.env.GITHUB_REPO;
+
+  await fetch(`https://api.github.com/repos/${owner}/${repo}/dispatches`, {
+    method: "POST",
+    headers: {
+      "Accept": "application/vnd.github+json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      event_type: action,
+      client_payload: payload
+    })
+  });
+
+  return new Response("OK");
+}
